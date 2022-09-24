c565a3d44ae51fcace3e2afb1b5bff16126438a5
Ildar <mrlaax@gmail.com>
2022-09-24 12:45:03 +0300

    продолжения урока

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-console.log ("Hello world")
+console.log("Hello world");
+
+/* let onePlanetName = "Zemly"; Переменая 
+console.log(onePlanetName);
+ */
+let newTime = "12:16";
+console.log(newTime);
+
+let amountChater = "19";
+console.log(amountChater);
+
+let lastName = "ILdar";
+console.log(lastName);
+
+let SarName = "Musin";
+console.log(SarName);
+
+let patronymicNew = "Ilshatovic";
+console.log(patronymicNew);
+
+const UserData = "Имя пользователя ${name}, возраст {age}";
+console.log(UserData)
+
+const UserDatanew = "Имя пользователь ${name}, возраст {age}";
+console.log(UserDatanew)
 
 
-let onePlanetName = "Zemly"
-console.log(onePlanetName)
+/* Boolean
+const isUser = false Ложь
+const isVisible = true Истина */
 
-let newTime = "12:16"
-console.log(newTime)
+let townName;
+console.log(townName)
+townName = "Moscow"
+console.log(townName);
 
-let amountChater = "19"
-console.log(amountChater)
+// const UserData ={
+// name: "Ildar",
+// age: "32",
+// };
+// // console.log(UserData.name);
 
-let lastName = "ILdar"
-console.log(lastName)
+console.log(typeof UserData.age);
 
-let SarName = "Musin"
-console.log(SarName)
 
-let patronymicNew ="Ilshatovic"
-console.log(patronymicNew)
+
+
