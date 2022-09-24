1f009c17fbdf8ecc0129dad0c93f31a1d6a1f576
Ildar <mrlaax@gmail.com>
2022-09-24 10:42:04 +0300

    добавил файл первого занятие

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log ("Hello world")
+
+
+let onePlanetName = "Zemly"
+console.log(onePlanetName)
+
+let newTime = "12:16"
+console.log(newTime)
+
+let amountChater = "19"
+console.log(amountChater)
+
+let lastName = "ILdar"
+console.log(lastName)
+
+let SarName = "Musin"
+console.log(SarName)
+
+let patronymicNew ="Ilshatovic"
+console.log(patronymicNew)
